17691a314d3dc2ec56382207da4229f8c0c7e0aa
Kevin Lundberg <klundberg@wayfair.com>
2018-12-06 01:56:15 -0500

    Eliminate warnings for unused solution functions

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,13 @@ mod util;
 extern crate edit_distance;
 
 fn main() {
-    // aoc2018::day1::solution();
-    aoc2018::day2::solution();
+    let solutions = [
+        aoc2018::day1::solution,
+        aoc2018::day2::solution,
+    ];
+    
+    // run the latest 
+    let latest = solutions.last().unwrap();
+    latest();
 }
 
